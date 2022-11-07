f6db759c17de634f4ffb29526645d2fc38c59003
Tom Chapman <tomchapman128@gmail.com>
2022-11-07 19:12:36 +0000

    Add board generation and cell click

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ screens.forEach(screen => {
 });
 
 
+function cellClicked(r, c) {
+    console.log(`Cell clicked: row ${r}, column ${c}`);
+}
+
+
+function generateBoard(x) {
+    const board = document.getElementById('board');
+    
+    for (let i = 0; i < x; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < x; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            cell.addEventListener('click', () => {
+                cellClicked(i, j);
+            });
+
+            cell.innerHTML = `<span class="debug-info">${i}, ${j}</span>`;
+
+            row.appendChild(cell);
+        }
+        board.appendChild(row);
+    }
+}
+
+
+
 function showScreen(screenName) {
     for (let screen in screenDict) {
         screenDict[screen].classList.remove('active');
@@ -19,4 +48,4 @@ function startGame() {
 }
 
 showScreen('menu');
-
+generateBoard(3);
